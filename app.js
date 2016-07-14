d167256c7cff8075e3ced9618397a6c42b05d5da
Andrew Glibbery <andrew.glibbery@gmail.com>
2016-07-14 18:04:32 -0400

    adds static students

---
+++ app.js
@@ -0,0 +1,57 @@
+var app = angular.module('tardyTrackerApp', ['ui.router']);
+
+app.factory('classes', [function() {
+	var o = {
+		classes: []
+	};
+	return o;
+}]);
+
+app.controller('MainCtrl', [
+'$scope',
+'classes',
+function($scope, classes){
+  $scope.classes = classes.classes;
+
+	$scope.addClass = function() {
+		if(!$scope.className || $scope.className === '') { return; };
+			$scope.classes.push({
+				classPeriod: $scope.className,
+				students: [
+				{name: 'Alexa'}
+				]
+			});
+		$scope.className = '';
+	};
+
+}]);
+
+app.controller('ClassesCtrl', [
+'$scope',
+'$stateParams',
+'classes',
+function($scope, $stateParams, classes){
+	$scope.class = classes.classes[$stateParams.id];
+
+}]);
+
+app.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: '/home.html',
+      controller: 'MainCtrl'
+    })
+
+    .state('classes', {
+	  url: '/classes/{id}',
+	  templateUrl: '/classes.html',
+	  controller: 'ClassesCtrl'
+	});
+
+  $urlRouterProvider.otherwise('home');
+}]);
